131f843b2593ed4fdf94b079736fabaedde6cb9c
davidhavl <contact@davidhavl.com>
2019-02-28 15:08:29 +0000

    Add version to api routes

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -3,16 +3,16 @@ import { Router, Response, Request, NextFunction } from 'express'
 import { itemRoutes } from '@/modules/item/config/routes'
 
 const router = Router()
+const version = process.env.API_VERSION || 'v1'
 
-router.get('/', (req: Request, res: Response, next: NextFunction): void => {
-  // TODO: recode to use boom?
+router.get('/', (req: Request, res: Response): void => {
   // setTimeout(() => {
   //   throw new Error('Oops this is caught where')
   // }, 1000)
-  throw new Error('This is a normal error')
-
+  // throw new Error('This is a normal error')
+  res.status(200).send('OK')
 })
 
-router.use('/item', itemRoutes)
+router.use(`/${version}`, itemRoutes)
 
 export default router
