b9d7ffc2c098fb380b568b8fecded2399d9219f2
davidhavl <contact@davidhavl.com>
2019-02-28 15:48:52 +0000

    Adjust app methods

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
+/**
+ * @author     David Havl info@davidhavl.com
+ * @license    MIT
+ */
 /* tslint:disable:semicolon */
 import express, { Response, Request, NextFunction } from 'express'
 import bodyParser from 'body-parser'
 import { config } from 'dotenv'
 import compression from 'compression'  // compresses requests
 import 'express-async-errors'
-import joiErrorMiddleware from './middleware/joiErrorMiddleware'
+import httpErrorHandler from '@/lib/middleware/httpErrorHandler'
 import prettyError from 'pretty-error'
 import helmet from 'helmet'
 import morgan from 'morgan'
@@ -15,14 +19,13 @@ import CacheServiceInterface from '@/modules/core/services/interfaces/CacheServi
 
 // custom
 import routes from '@/config/routes'
-
 class App {
   public express: express.Express
   private logger: LogServiceInterface
   private db: DbServiceInterface
   private cache: CacheServiceInterface
 
-  constructor () {
+  constructor() {
     // Load environment variables from .env file, where API keys and passwords are configured
     config({ path: '.env' })
     // set express
@@ -38,27 +41,26 @@ class App {
     // initStaticFiles()
     //
     this.initCors()
-    // routes
-    this.initRoutes()
     // logging
     this.logger = DIContainer.get<LogServiceInterface>(DITypes.LogService)
     this.db = DIContainer.get<DbServiceInterface>(DITypes.DbService)
     this.cache = DIContainer.get<CacheServiceInterface>(DITypes.CacheService)
     this.initRequestLogging()
-
+    // routes
+    this.initRoutes()
     // errors
-    this.initErrorHandler()
+    this.initErrorHandling()
   }
 
-  private initRoutes (): void {
+  private initRoutes(): void {
     this.express.use('/', routes)
   }
 
-  private initStaticFiles (): void {
+  private initStaticFiles(): void {
     this.express.use(express.static('static'))
   }
 
-  private initCors (): void {
+  private initCors(): void {
     // set CORS headers
     this.express.use((req: Request, res: Response, next: NextFunction) => {
       res.header('Access-Control-Allow-Origin', process.env.CORS_ORIGIN)
@@ -78,16 +80,15 @@ class App {
     }
   }
 
-  private initErrorHandler(): void {
+  public initErrorHandling(): void {
     const pe = new prettyError()
     pe.skipNodeFiles()
     pe.skipPackage('express')
     pe.skipPackage('express-async-errors')
 
-    // errors returned by Joi input validators.
-    this.express.use(joiErrorMiddleware)
-    // intercepting async errors and sending them to next(err) so it turns up here
-    // is done via express-async-errors module automatically
+    // errors returned as validation or that should be send back to user.
+    this.express.use(httpErrorHandler)
+    // intercepting async errors and sending them to next(err) so it turns up here is done via express-async-errors
     this.express.use((err: any, req: Request, res: Response, next: NextFunction) => {
       // log
       if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
@@ -101,14 +102,13 @@ class App {
       // set status code
       res.status(err.statusCode || 500)
       // return problem-json formated response. https://tools.ietf.org/html/rfc7807
-      res.json({
+      res.send({
         status: err.statusCode || 500, // status code
         detail: err.message || 'Oops something went wrong!', // readable by users
-        title: err.name, // readable by engineers,
-        type: null, // URI pointing to webpage with that error
-        instance: null, // URI pointing to this specific error instance if any exists
+        title: err.name || 'Error', // readable by engineers,
         data: err.data || {}, // custom data
-      }).send()
+      })
+      // TODO: this is probably 500 error so should we restart the server?
     })
   }
 
@@ -116,7 +116,11 @@ class App {
     return this.logger
   }
 
-  public cleanup (): void {
+  public getExpress() {
+    return this.express
+  }
+
+  public cleanup(): void {
     const actions: Function[] = [
       this.db.close,
       this.cache.close,
