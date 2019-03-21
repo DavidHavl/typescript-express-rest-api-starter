3e59e3f0a30ba943f09c5b7ebed0ef7c51bff80c
davidhavl <contact@davidhavl.com>
2019-03-21 00:02:22 +0000

    Remove comments

--- src/modules/item/controllers/ItemController.ts
+++ src/modules/item/controllers/ItemController.ts
@@ -12,50 +12,17 @@ export default class ItemController implements RestApiControllerInterface {
   constructor () {}
 
   indexAction (req: Request, res: Response, next: NextFunction) {
-    // const items = Item.findAll()
-    // const itemData: object[] = []
-    // items.forEach((item: ModelInterface) => {
-    //   itemData.push(item.toExternalObject())
-    // })
-    // res.status(200).send(itemData)
   }
 
   getAction (req: Request, res: Response, next: NextFunction) {
-    // const id = req.params.id
-    // const item = Item.findById(id)
-    // if (!item || !item.id) {
-    //   return Boom.wrongItem
-    // }
-    // res.status(200).send(item.toExternalObject())
   }
 
-  async postAction (req: Request, res: Response, next: NextFunction) {
-    // const validator = new ItemInputValidator(req.body)
-    // if (!validator.isValid()) {
-    //   return new ValidationError(validator.getErrors())
-    // }
-    // const item = new Item(validator.getCleanData());
-    // await item.save()
-    // res.status(200).send(item.toExternalObject())
+  postAction (req: Request, res: Response, next: NextFunction) {
   }
 
   patchAction (req: Request, res: Response, next: NextFunction) {
-    // const id = req.params.id
-    // const item = Item.findById(id)
-    // if (!item) {
-    //   return new NotFoundError()
-    // }
-    // const validator = new ItemInputValidator(req.body)
-    // if (!validator.isValid()) {
-    //   return new ValidationError(validator.getErrors())
-    // }
-    // const validData = validator.getCleanData()
-    // item.populate(validData)
-    // item.save()
-    res.status(200).send('OK')
   }
 
   deleteAction (req: Request, res: Response, next: NextFunction) {
-    res.status(200).send('OK')
   }
 }
