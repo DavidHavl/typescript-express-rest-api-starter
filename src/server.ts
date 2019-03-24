b823c0e997b4f3d7fbe60de2225d9b05cec494dc
davidhavl <contact@davidhavl.com>
2019-03-24 18:58:55 +0000

    Rewrite server.js for new flow

--- src/server.ts
+++ src/server.ts
@@ -2,34 +2,12 @@
  * @author     David Havl info@davidhavl.com
  * @license    MIT
  */
-import app from './app'
+import App from './app'
 
 const PORT = process.env.PORT || 3000
 
-const server = app.express.listen(+PORT, (err: Error) => {
-  if (err) {
-    app.getLogger().error(err)
-    return
-  }
-
-  return console.log(`server is listening on port ${PORT}`)
-})
-
-function closeAndExit(options: { exit?: boolean, cleanup?: boolean}, err: Error | {} | undefined): void {
-  // cleanup and exit
-  if (options.cleanup) {
-    server.close()
-    app.cleanup()
-  }
-  // log error
-  if (err) {
-    app.getLogger().error(err)
-  }
-  // exit
-  if (options.exit) {
-    process.exit()
-  }
-}
+const app = new App()
+app.start(+PORT)
 
 // for unhandled rejections
 process.on('unhandledRejection', (ex, promise) => {
@@ -37,9 +15,18 @@ process.on('unhandledRejection', (ex, promise) => {
 })
 
 // for errors thrown outside of express route lifecycle
-process.on('uncaughtException', closeAndExit.bind(null, { exit: true }))
+process.on('uncaughtException', (err) => {
+  app.getLogger().error(err)
+  process.exit(1)
+})
 
 // other process events
-process.on('exit', closeAndExit.bind(null, { cleanup: true }))
-process.on('SIGINT', closeAndExit.bind(null, { exit: true }))
-process.on('SIGTERM', closeAndExit.bind(null, { exit: true }))
+process.on('exit', () => {
+  app.stop()
+})
+process.on('SIGINT', () => {
+  process.exit(1)
+})
+process.on('SIGTERM', () => {
+  process.exit(1)
+})
